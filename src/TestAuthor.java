99f5210ac070cbe7898f0c811fc3ec58a200edfe
andra <andras_kapitany@hotmail.hu>
2023-06-12 07:10:45 +0200

    TestAutrhor calss updated

--- src/TestAuthor.java
+++ src/TestAuthor.java
@@ -1,2 +1,8 @@
 public class TestAuthor {
+    public static void main(String[] args) {
+        Author kBela = new Author("Kis Béla","bela.kis@gmal.com",'m');
+        System.out.println("name is: " + kBela.getName());
+        System.out.println("email is: " + kBela.getEmail());
+        System.out.println("gender is: " + kBela.getGender());
+    }
 }
